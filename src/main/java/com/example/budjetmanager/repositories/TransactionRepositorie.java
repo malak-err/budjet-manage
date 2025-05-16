ca6e542a32d2c39003902b3e73339e6fa3c274d0
hdfaouz <hdfaouzia2@gmail.com>
2025-05-16 15:12:05 +0100

    Fichier README

--- src/main/java/com/example/budjetmanager/repositories/TransactionRepositorie.java
+++ src/main/java/com/example/budjetmanager/repositories/TransactionRepositorie.java
@@ -13,5 +13,5 @@ import java.util.Optional;
 public interface TransactionRepositorie extends JpaRepository<Transaction,Long> {
 
 
-    Optional<List<Transaction>> findTransactionsByCategorie(Categorie categorie);
+
 }
